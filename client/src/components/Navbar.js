7fc8cf6883960b99469dbf380ddb88d8610aefe6
Imesh Pasinda <imeshpasinda@gmail.com>
2022-12-17 13:17:19 +0530

    Update Nav bar V1.1

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg shadow-lg p-3 mb-5 bg-white rounded ">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">HUNGRY-MEALS</a>
+                    <a className="navbar-brand" href="#">HUNGRY MEALS</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
