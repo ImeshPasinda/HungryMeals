55d27946b381c6197e4190ee0a2d14bb89f5bb23
Imesh Pasinda <imeshpasinda@gmail.com>
2022-12-17 18:31:38 +0530

    Created HomeScreen

---
+++ client/src/pizzasdata.js
@@ -0,0 +1,100 @@
+const pizzas = [
+
+    {
+
+        name: "PEPPER BARBECUE CHICKEN",
+        varients: ["small", "medium", "large"],
+        prices: [
+            {
+                small: 200,
+                medium: 350,
+                large: 400,
+            },
+        ],
+        category: "nonveg",
+        image: "https://adminsc.pizzahut.lk//images/mainmenu/696c5a13-d652-4f83-b784-52a4f81ef68b.jpg",
+        description: "Pepper Barbecue Chicken Cheese",
+    },
+
+    {
+
+        name: "PEPPER BARBECUE CHICKEN",
+        varients: ["small", "medium", "large"],
+        prices: [
+            {
+                small: 200,
+                medium: 350,
+                large: 400,
+            },
+        ],
+        category: "nonveg",
+        image: "https://adminsc.pizzahut.lk//images/mainmenu/696c5a13-d652-4f83-b784-52a4f81ef68b.jpg",
+        description: "Pepper Barbecue Chicken Cheese",
+    },
+
+    {
+
+        name: "PEPPER BARBECUE CHICKEN",
+        varients: ["small", "medium", "large"],
+        prices: [
+            {
+                small: 200,
+                medium: 350,
+                large: 400,
+            },
+        ],
+        category: "nonveg",
+        image: "https://adminsc.pizzahut.lk//images/mainmenu/696c5a13-d652-4f83-b784-52a4f81ef68b.jpg",
+        description: "Pepper Barbecue Chicken Cheese",
+    },
+
+    {
+
+        name: "PEPPER BARBECUE CHICKEN",
+        varients: ["small", "medium", "large"],
+        prices: [
+            {
+                small: 200,
+                medium: 350,
+                large: 400,
+            },
+        ],
+        category: "nonveg",
+        image: "https://adminsc.pizzahut.lk//images/mainmenu/696c5a13-d652-4f83-b784-52a4f81ef68b.jpg",
+        description: "Pepper Barbecue Chicken Cheese",
+    },
+
+    {
+
+        name: "PEPPER BARBECUE CHICKEN",
+        varients: ["small", "medium", "large"],
+        prices: [
+            {
+                small: 200,
+                medium: 350,
+                large: 400,
+            },
+        ],
+        category: "nonveg",
+        image: "https://adminsc.pizzahut.lk//images/mainmenu/696c5a13-d652-4f83-b784-52a4f81ef68b.jpg",
+        description: "Pepper Barbecue Chicken Cheese",
+    },
+
+    {
+
+        name: "PEPPER BARBECUE CHICKEN",
+        varients: ["small", "medium", "large"],
+        prices: [
+            {
+                small: 200,
+                medium: 350,
+                large: 400,
+            },
+        ],
+        category: "nonveg",
+        image: "https://adminsc.pizzahut.lk//images/mainmenu/696c5a13-d652-4f83-b784-52a4f81ef68b.jpg",
+        description: "Pepper Barbecue Chicken Cheese",
+    },
+];
+
+export default pizzas
